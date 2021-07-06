ade988446aa7cfb51a07df10de45ce52cc64f59a
kurrbanov <justnick1312@gmail.com>
2021-07-06 10:02:26 +0300

    Made views and urls.

--- config/museum/views.py
+++ config/museum/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+from .models import Picture, Author
+
+
+def main_page(request):
+    return render(request, 'main.html')
+
+
+def all_pictures_view(request):
+    pictures = Picture.objects.all()
+    return render(request, 'list.html', context={'pictures': pictures, 'authors': None})
+
+
+def all_authors_view(request):
+    authors = Author.objects.all()
+    return render(request, 'list.html', context={'pictures': None, 'authors': authors})
+
+
+def show_picture(request, pk):
+    try:
+        picture = Picture.objects.get(id=pk)
+    except Picture.DoesNotExist:
+        return HttpResponse(request, 'Картина не существует')
+
+    return render(request, 'info_html.html', context={'picture': picture})
+
+
+def show_author(request, pk):
+    try:
+        author = Author.objects.get(id=pk)
+    except Author.DoesNotExist:
+        return HttpResponse(request, 'Автор не существует')
+
+    return render(request, 'info_html.html', context={'author': author})
